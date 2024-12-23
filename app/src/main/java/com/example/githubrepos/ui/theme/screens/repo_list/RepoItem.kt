17ff9849a55eb61c18e0e658a5c048a71005ba3e
shahdgaballah <shahdgabllah@gmail.com>
2024-12-23 18:50:08 +0200

    second commit: part 2 : RepoItem

--- app/src/main/java/com/example/githubrepos/ui/theme/screens/repo_list/RepoItem.kt
+++ app/src/main/java/com/example/githubrepos/ui/theme/screens/repo_list/RepoItem.kt
@@ -12,30 +12,41 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.githubrepos.R
 import com.example.githubrepos.ui.theme.GithubReposTheme
+import com.example.githubrepos.ui.theme.Yellow
+import com.example.githubrepos.ui.theme.screens.repo_list.preview.fakeRepoList
 
 @Composable
-fun RepoItem(modifier: Modifier = Modifier) {
+fun RepoItem(
+    repoItemUiModel: GithubRepoUiModel,
+    modifier: Modifier = Modifier,
+    itemClicked: () -> Unit
+             ) {
     Row (
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 16.dp)
             .padding(top = 12.dp)
-            .background(color = Color.White, shape = RoundedCornerShape(12.dp))
+            .background(
+                color = MaterialTheme.colorScheme.surface,
+                shape = MaterialTheme.shapes.medium
+            )
             .clickable {
                 //handle clicked repo item
+                itemClicked()
             }
     ) {
         Image(
@@ -57,21 +68,35 @@ fun RepoItem(modifier: Modifier = Modifier) {
             ) {
                 Text(
                     modifier = Modifier.weight(1f),
-                    text = "kotlin repository")
+                    text = repoItemUiModel.name, //stringResource(R.string.repo_name),
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.titleMedium
+                )
 
-                Text(text = "00000")
+                Text(text = repoItemUiModel.stars, //stringResource(R.string.stars_count),
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.bodyMedium
+                    )
                 Image(
                     modifier = Modifier
-                        .size(30.dp),
-                        painter = painterResource(R.drawable.ic_launcher_foreground),
+                        .size(20.dp),
+                        painter = painterResource(R.drawable.ic_star),
+                    colorFilter = ColorFilter.tint(Yellow),
                         contentDescription = null
                     )
             }
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text= "google")
+            Text(
+                repoItemUiModel.owner,
+                //text= stringResource(R.string.repo_owner),
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.bodyMedium
+                )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
-                text="description description description descriptionnnnnnnnnnnnnnnnnn.",
+                text= repoItemUiModel.description, // stringResource(R.string.repo_description),
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.bodyMedium,
                 maxLines = 2,
                 overflow = TextOverflow.Ellipsis
             )
@@ -86,7 +111,7 @@ fun RepoItem(modifier: Modifier = Modifier) {
 @Composable
 private fun PreviewRepoItem() {
     GithubReposTheme {
-        RepoItem()
+        RepoItem(repoItemUiModel = fakeRepoList.first(), itemClicked = {})
     }
     
 }
